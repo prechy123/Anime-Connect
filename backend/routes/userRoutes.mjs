32837db33b1e9549e88234bb999f40c6be2bbc83
prechy123 <bamideleprecious100@gmail.com>
2023-09-06 19:27:04 +0100

    integrated request-ip to /user/signin route

--- backend/routes/userRoutes.mjs
+++ backend/routes/userRoutes.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import { signupSigninLimiter } from "../middleware/limiter/limiter.mjs";
 import { addUserValidationHandler, addUserValidator } from "../middleware/users/usersValidator.mjs";
 import { createUser } from "../controllers/userController.mjs";
+import requestIp from "request-ip"
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.post(
   addUserValidationHandler,
   createUser
 );
-router.post("/signin", signupSigninLimiter, )
+//added client ip addredd to request object as req.clientIp
+router.post("/signin", signupSigninLimiter, requestIp.mw())
 
 export default router;
