e9bb87f968f6206d55718e5c3e218a6ac3392bd7
prechy123 <bamideleprecious100@gmail.com>
2023-09-06 19:44:17 +0100

    Added useragent-express

--- backend/routes/userRoutes.mjs
+++ backend/routes/userRoutes.mjs
@@ -1,8 +1,14 @@
 import express from "express";
 import { signupSigninLimiter } from "../middleware/limiter/limiter.mjs";
-import { addUserValidationHandler, addUserValidator } from "../middleware/users/usersValidator.mjs";
+import {
+  addUserValidationHandler,
+  addUserValidator,
+} from "../middleware/users/usersValidator.mjs";
 import { createUser } from "../controllers/userController.mjs";
-import requestIp from "request-ip"
+
+//middleware
+import requestIp from "request-ip";
+import useragent from "express-useragent";
 
 const router = express.Router();
 
@@ -14,6 +20,12 @@ router.post(
   createUser
 );
 //added client ip addredd to request object as req.clientIp
-router.post("/signin", signupSigninLimiter, requestIp.mw())
+//added useragent to keep track of browsers and device
+router.post(
+  "/signin",
+  signupSigninLimiter,
+  requestIp.mw(),
+  useragent.express()
+);
 
 export default router;
