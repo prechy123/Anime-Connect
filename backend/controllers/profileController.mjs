07f53abab3e5c5e792ab78b2e5167a4a031303db
prechy123 <bamideleprecious100@gmail.com>
2023-09-13 15:40:17 +0100

    using $addToSet

--- backend/controllers/profileController.mjs
+++ backend/controllers/profileController.mjs
@@ -15,19 +15,30 @@ import Relationship from "../models/relationshipModel.mjs"
 
 
 
-export const followUser = (req, res) => {
+export const followUser = async (req, res) => {
     try {
         const followerId = res.userId
         const followingId = req.params.id
 
-        const relationshipExist = Relationship.exists({
+        const relationshipExist = await Relationship.exists({
             follower: followerId,
             following: followingId
         })
-        
         if (relationshipExist) {
             return res.status(400).json({message: "Already following this user"})
         }
+
+        await Promise.all([
+            User.findByIdAndUpdate(
+                followingId,
+                {$addToSet: {followers: followerId}}
+            ),
+            User.findByIdAndUpdate(
+                followerId,
+                {$addToSet: {following: followingId}}
+            )
+        ])
+
     } catch(error) {
 
     }
