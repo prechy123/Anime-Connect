3c5ca0d1c58ccd3831c154cf306090f113b14ca0
prechy123 <bamideleprecious100@gmail.com>
2023-09-13 15:43:34 +0100

    followUser

--- backend/controllers/profileController.mjs
+++ backend/controllers/profileController.mjs
@@ -1,49 +1,32 @@
-import Relationship from "../models/relationshipModel.mjs"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import Relationship from "../models/relationshipModel.mjs";
 
 export const followUser = async (req, res) => {
-    try {
-        const followerId = res.userId
-        const followingId = req.params.id
+  try {
+    const followerId = res.userId;
+    const followingId = req.params.id;
 
-        const relationshipExist = await Relationship.exists({
-            follower: followerId,
-            following: followingId
-        })
-        if (relationshipExist) {
-            return res.status(400).json({message: "Already following this user"})
-        }
-
-        await Promise.all([
-            User.findByIdAndUpdate(
-                followingId,
-                {$addToSet: {followers: followerId}}
-            ),
-            User.findByIdAndUpdate(
-                followerId,
-                {$addToSet: {following: followingId}}
-            )
-        ])
-
-    } catch(error) {
-
+    const relationshipExist = await Relationship.exists({
+      follower: followerId,
+      following: followingId,
+    });
+    if (relationshipExist) {
+      return res.status(400).json({ message: "Already following this user" });
     }
-  
-}
-export const unFollowUser = (req, res) => {
 
-}
+    await Promise.all([
+      User.findByIdAndUpdate(followingId, {
+        $addToSet: { followers: followerId },
+      }),
+      User.findByIdAndUpdate(followerId, {
+        $addToSet: { following: followingId },
+      }),
+    ]);
+    await Relationship.create({ follower: followerId, following: followingId });
+    res.status(200).json({ message: "User followe successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Some error occurred while following the user" });
+  }
+};
+export const unFollowUser = (req, res) => {};
