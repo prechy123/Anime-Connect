686c976cc748d430e8ac170822e732afc8d44661
prechy123 <bamideleprecious100@gmail.com>
2023-10-01 22:28:09 +0100

    ...

--- backend/middleware/limiter/limiter.mjs
+++ backend/middleware/limiter/limiter.mjs
@@ -2,6 +2,7 @@ import { rateLimit } from "express-rate-limit";
 const MESSAGE =
   "Too many accounts created from this IP, please try again after an hour";
 
+
 const createLimiter = (windowMs, max, message) => {
   return rateLimit({
     windowMs,
