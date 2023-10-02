61c679ab244a0461afd542d031dd6b9341512555
prechy123 <bamideleprecious100@gmail.com>
2023-10-02 22:01:11 +0100

    implemented decodeToken

--- backend/middleware/auth/decodeAuth.mjs
+++ backend/middleware/auth/decodeAuth.mjs
@@ -1,18 +1,11 @@
 import jwt from "jsonwebtoken";
 
 
-
-/**
- * NOTE: This middleware for decoding JWT is not necessary when using Passport's JWT strategy.
- * Passport handles token decoding and user extraction automatically.
- */
-
 const decodeToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.SECRET);
-    console.log(decoded)
     req.userId = decoded.id;
     next();
   } catch (err) {
@@ -20,4 +13,4 @@ const decodeToken = (req, res, next) => {
   }
 };
 
-export default decodeToken
+export default decodeToken;
