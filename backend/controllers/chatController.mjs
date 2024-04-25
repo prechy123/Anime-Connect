a1560a76d6ff2cbdd1fd1d4868bda0723aca0362
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-25 16:40:59 +0100

    chat controllers

--- backend/controllers/chatController.mjs
+++ backend/controllers/chatController.mjs
@@ -14,3 +14,30 @@ export const getChat = async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+export const sendMessages = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { message } = req.body;
+    const newMessage = {
+      sender: req.user._id,
+      content: message,
+      timestamp: new Date(),
+    };
+
+    const chat = await Chat.findOneAndUpdate(
+      {
+        $or: [
+          { participants: [req.user._id, userId] },
+          { participants: [userId, req.user._id] },
+        ],
+      },
+      { $push: { messages: newMessage } },
+      { upsert: true, new: true }
+    );
+
+    res.status(201).json(chat);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
