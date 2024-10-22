a673ac4ddc39a2460dfff4678f4c28e799ce37c9
prechy123 <bamideleprecious100@gmail.com>
2024-10-22 16:06:06 +0100

    update base url

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,5 +1,6 @@
 // const BASE_URL = "http://localhost:4000";
 
-const BASE_URL = "https://weeebs.onrender.com"
+// const BASE_URL = "https://weeebs.onrender.com"
+const BASE_URL = "https://weeebs-nkm3.onrender.com"
 
 export default BASE_URL;
